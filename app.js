8e1f9ee5fe2bf42ff2e1eefebb6eb5f2ac42ce9a
Michael Washburn <washburn.michael@gmail.com>
2017-12-28 11:15:20 -0800

    install google auth

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const app = express();
 
+// Passport config
+require('./config/passport')(passport);
+
+// Load Routes
+const auth = require('./routes/auth');
 // Routes
 app.get('/', (req, res) => {
   res.send('You made to the root page');
 });
+
+// Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://DevUser:DevUser1234.mlab.com:33657/storybooks-dev',
+  googleClientID: '1018339080938-ue29dvcrm1t7sutk3r74mja7gb75mvsk.apps.googleusercontent.com',
+  googleClientSecret:'c_SODU9zAsK2zqX1RcCRi3dc'
+}
