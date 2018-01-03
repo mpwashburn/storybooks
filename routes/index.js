0f16c4b012627daf072c9bc6b35e8850d6e3ad4d
Michael Washburn <washburn.michael@gmail.com>
2018-01-03 15:02:23 -0800

    add missing verify code

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('It Works!');
-});
-
-router.get('/welcome', (req, res) => {
   res.render('index/welcome');
 });
 
